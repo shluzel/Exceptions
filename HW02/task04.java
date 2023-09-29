d4de41590d7d90a81c2d8152c7ea839e3b6ae9fe
Darya <dasha.890@yandex.ru>
2023-09-29 18:27:34 +0500

    task04

--- HW02/task04.java
+++ HW02/task04.java
@@ -1,5 +1,17 @@
 package HW02;
+import java.util.Scanner;
 
 public class task04 {
-    
+    public static void main(String[] args) {
+        System.out.println("Введите данные:");
+        Scanner sc = new Scanner(System.in);
+        EmptyStringException(sc.nextLine());
+        sc.close();
+    }
+
+    public static void EmptyStringException(String stringValue) {
+        if (stringValue.isEmpty()) {
+            throw new RuntimeException("Вы ничего не ввели :с");
+        }
+    }
 }
